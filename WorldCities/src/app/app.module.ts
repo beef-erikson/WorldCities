da817d36b35006bace5936e3843b8cca94202651
beef-erikson <beef.erikson.studios@gmail.com>
2023-03-29 16:55:31 -0400

    Project skeleton set up and functioning.

--- WorldCities/src/app/app.module.ts
+++ WorldCities/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { NavMenuComponentComponent } from './nav-menu-component/nav-menu-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponentComponent,
-    NavMenuComponentComponent
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule,
